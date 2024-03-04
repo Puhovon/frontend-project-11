94c79272718dd87390455bdfa3b2e241cd7846d2
Puhovon <pv487732@gmail.com>
2024-03-04 23:58:07 +0300

    Second step

--- src/components/app.js
+++ src/components/app.js
@@ -2,44 +2,56 @@ import 'bootstrap';
 import onChange from 'on-change';
 import * as yup from 'yup';
 
-const urlValidate = async (url) => yup.string().required().url().notOneOf(url);
+const formState = {
+  valid: 'valid',
+  invalid: 'invalid',
+  download: 'download',
+};
+
+const validate = (state, inputVal) => {
+  const shema = yup.string().required().url().notOneOf(state.rssForm.urls);
+  return shema.validate(inputVal);
+};
 
 export default () => {
-    console.log("AAAAA")
-    console.log("AAAAA")
-    const state = {
-        rssForm: {
-            valid: 'valid',
-            urls: [],
-            errors: [],
-        },
-        rssResults: {
-            urls: [],
-        }
+  const state = {
+    rssForm: {
+      valid: 'filling',
+      urls: [],
+      errors: [],
+    },
+    rssResults: {
+      urls: [],
+    },
+  };
+
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'rssForm.valid') {
+      switch (value) {
+        case formState.valid:
+          console.log('valid');
+          break;
+        case formState.invalid:
+          console.log('invalid');
+          break;
+        default:
+          break;
+      }
     }
+  });
 
-
-    const watchedState = onChange(state, (path, value) => {
-        if (path === 'rssForm') {
-            switch (value) {
-                case 'valid':
-                break;
-                case 'invalid':
-                break;
-                case 'filling':
-                break;
-            }
-        }
+  const form = document.querySelector('form');
+  const input = document.querySelector('#rss-url');
+  const getUrl = async (e) => {
+    e.preventDefault();
+    const url = input.value;
+    validate(state, url).then(() => {
+      watchedState.rssForm.urls.push(url);
+      watchedState.rssForm.valid = formState.valid;
     })
-    const form = document.querySelector('form');
-    const getUrl = async (e) => {
-        console.log("HUY")
-        e.preventDefault();
-        const { target } = e;
-        const url = target.value
-        watchedState.rssForm.urls.push(url);
-         
-        console.log(await urlValidate()).isValid(watchedState.rssForm.urls);
-    };
-    form.addEventListener('submit', getUrl);
+      .catch(() => {
+        watchedState.rssForm.urls.valid = formState.invalid;
+      });
+  };
+  form.addEventListener('submit', getUrl);
 };
