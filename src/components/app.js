340de5520ad475a354e31d2b55f057b70f1746db
Puhovon <pv487732@gmail.com>
2024-03-04 18:27:05 +0300

    firstStep

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,13 @@
 import 'bootstrap';
 
 export default () => {
-    console.log('Hey')
+    const form = document.querySelector('form');
+    const getUrl = (e) => {
+        e.preventDefault();
+        const target = e.target;
+        const data = new FormData(target);
+        const url = new URL(data.get('url'));
+        console.log(url)
+    }
+    form.addEventListener('submit', getUrl)
 }
